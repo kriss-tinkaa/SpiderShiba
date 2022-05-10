a5bd671040bf82a3819cd857befc85259ee72a1a
kradchenkonlt <44407472+kriss-tinkaa@users.noreply.github.com>
2022-05-10 13:04:38 +0200

    Added response to the main section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
+$(document).ready(function () {
 
-$('.slider').slick({
-    dots: true,
-    infinite: false,
-    autoplay: true,
-    prevArrow: false,
-    nextArrow: false
+    $("#mobile-menu").click(function () {
+        $(this).toggleClass("active");
+        $('.menu').toggleClass("active");
+        $('.header').toggleClass("active");
+    });
+
+    $('.slider').slick({
+        dots: true,
+        infinite: false,
+        autoplay: true,
+        prevArrow: false,
+        nextArrow: false
+    });
+
+
+    
 });
+
