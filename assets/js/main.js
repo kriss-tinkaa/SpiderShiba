c682b639276cbe6bf8358c3398a3f5a82a3a94c5
kradchenkonlt <44407472+kriss-tinkaa@users.noreply.github.com>
2022-05-12 13:06:30 +0200

    Add roadmap

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         }
     });
 
-    $('.slider').slick({
+    $('.team-slider').slick({
         dots: true,
         infinite: false,
         autoplay: true,
@@ -25,15 +25,43 @@ $(document).ready(function () {
 
 
     
-});
+    var items = $(".timeline-card-row"),
+    timelineHeight = $(".timeline-cards").height(),
+    greyLine = $('.default-line'),
+    lineToDraw = $('.draw-line');
 
+if(lineToDraw.length) {
+  $(window).on('scroll', function () {
 
+    // Need to constantly get '.draw-line' height to compare against '.default-line'
+    var redLineHeight = lineToDraw.height(),
+    greyLineHeight = greyLine.height(),
+    windowDistance = $(window).scrollTop(),
+    windowHeight = $(window).height() / 2,
+    timelineDistance = $(".timeline").offset().top;
 
-let articles = document.getElementsByClassName('timeline-content');
+    if(windowDistance >= timelineDistance - windowHeight) {
+      line = windowDistance - timelineDistance + windowHeight;
 
-for(article of articles) {
-  let ar = article.getElementsByTagName("article");
-  let height = ar[0].clientHeight;
-  article.style.minHeight =  height - height/3 + "px";
-  console.log(height);
+      if(line <= greyLineHeight) {
+        lineToDraw.css({
+          'height' : line + 20 + 'px'
+        });
+      }
+    }
+
+    // This takes care of adding the class in-view to the li:before items
+    var bottom = lineToDraw.offset().top + lineToDraw.outerHeight(true);
+    items.each(function(index){
+      var circlePosition = $(this).offset();
+
+      if(bottom > circlePosition.top) {				
+        $(this).addClass('in-view');
+      } else {
+        $(this).removeClass('in-view');
+      }
+    });	
+  });
 }
+
+});
